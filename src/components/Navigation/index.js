e538db0bbce55fa84277e11f9703cb796ba6ac1a
ClauGarfiasMx <apps@claudiagarfias.works>
2019-08-06 02:47:04 -0500

    LogIn & logOut ready

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,27 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import { signout } from "../Auth";
 
-const Navigation = () => (
+const logOut = history => {
+  signout();
+  history.push("/signin");
+  window.location.reload();
+};
+
+const Navigation = ({ authUser, history }) => (
   <nav>
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ITEM_REGISTRY}>Register Item</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.INVENTORY}>Inventory</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.CONFIGURATION}>Configuration</Link>
-      </li>
+      {authUser ? (
+        <React.Fragment>
+          <li>
+            <Link to={ROUTES.ITEM_REGISTRY}>Register Item</Link>
+          </li>
+          <li>
+            <Link to={ROUTES.INVENTORY}>Inventory</Link>
+          </li>
+          <li>
+            <Link to={ROUTES.CONFIGURATION}>Configuration</Link>
+          </li>
+          <button
+            onClick={() => {
+              logOut(history);
+            }}
+          >
+            Sign Out
+          </button>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <li>
+            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+          </li>
+          <li>
+            <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+          </li>
+        </React.Fragment>
+      )}
     </ul>
   </nav>
 );
 
-export default Navigation;
+export default withRouter(Navigation);
